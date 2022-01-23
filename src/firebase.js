948d49f1dbdca2098cbd60390f1c82d5f1a2858e
brighton hoods <brightonmajiwa@gmail.com>
2022-01-23 21:05:23 +0300

    firebase installation

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCNXF0gPHl70Ob4Hx4GkJu7yxtlsL3r7Z0",
+    authDomain: "tinder-clone-9cb7a.firebaseapp.com",
+    projectId: "tinder-clone-9cb7a",
+    storageBucket: "tinder-clone-9cb7a.appspot.com",
+    messagingSenderId: "76933142184",
+    appId: "1:76933142184:web:f2d0645152ebb65840d49d",
+    measurementId: "G-KXRCLE1WEV"
+};
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const database = firebase.firestore();
+export default database;
