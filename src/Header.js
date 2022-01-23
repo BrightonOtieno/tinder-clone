0e105b9da259f70297d91ba6d1b4a99f456ca30f
brighton hoods <brightonmajiwa@gmail.com>
2022-01-23 17:12:30 +0300

    material ui install & header setup

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import PersonIcon from '@material-ui/icons/Person';
+import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
+import IconButton from '@material-ui/core/IconButton';
+
+import "./Header.css"
 
 function Header() {
     return (
-        <div>
+        <div className='header'>
+            <IconButton>
+                <PersonIcon  fontSize="large"  className="header__icon"/>
+            </IconButton>
+            
+            <img className='header__logo' src="https://logos-world.net/wp-content/uploads/2020/09/Tinder-Emblem.png" alt="tinder-logo" />
+            <IconButton>
+                <QuestionAnswerIcon fontSize="large"  className='header__icon'/>
+            </IconButton>
+            
+            
             
         </div>
     );
